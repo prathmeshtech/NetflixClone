156c7ac69448b2b90723c9b3ce27e8c905b64f02
Prathmesh Singh <64306983+prathmeshtech@users.noreply.github.com>
2021-12-07 14:47:15 +0530

    rows added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
-      <Row title= "NETFLIX ORIGINALS"/>
-      <Row title=" Trending Now"/>
+      <Row title= "NETFLIX ORIGINALS" 
+      fetchURL={requests.fetchNetflixOriginals}
+      isLargeRow
+      />
+      <Row title=" Trending Now" fetchURL={requests.fetchTrending}/>
+      <Row title=" Top Rated" fetchURL={requests.fetchTopRated}/>
+      <Row title=" Comedy" fetchURL={requests.fetchComedyMovies}/>
+      <Row title=" Horror" fetchURL={requests.fetchHorrorMovies}/>
+      <Row title=" Action" fetchURL={requests.fetchActionMovies}/>
+      <Row title=" Romance" fetchURL={requests.fetchRomanceMovies}/>
+      <Row title=" Documentry" fetchURL={requests.fetchDocumentaries}/>
     </div>
   );
 }
