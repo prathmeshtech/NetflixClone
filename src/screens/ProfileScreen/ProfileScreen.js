7a7da0098488429e947fec45c1c04891d85b0914
Prathmesh Singh <64306983+prathmeshtech@users.noreply.github.com>
2021-12-14 23:04:22 +0530

    commit 5

--- src/screens/ProfileScreen/ProfileScreen.js
+++ src/screens/ProfileScreen/ProfileScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import userSlice from '../../features/userSlice';
+import {useSelector} from "react-redux";
+import {userSlice} from '../../features/userSlice';
 import { auth } from '../../firebase';
 import Navbar from '../../Navbar/Navbar';
 import "./ProfileScreen.css";
@@ -14,13 +15,13 @@ const ProfileScreen = () => {
                     <img src = "https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
                     alt="netflix_profile" />
                 </div>
-                <div className='profileScreen_details'>
-                    <h2>{userSlice.email}</h2>
+                <div className="profileScreen_details">
+                    <h2>xyz@gmail.com</h2>
                     <div className='profileScreen_plans'>
-                        <button  onClick ={()=> auth.signOut()} className='profileScreen_signOut'>Sign Out</button>
+                        <button  onClick ={()=> auth.signOut()} className='profileScreen_signOut'>Sign Out</button>                        </div>
                     </div>
                 </div>
-            </div>
+            <div/>
         </div>
     )
 }
